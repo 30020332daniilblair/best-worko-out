9e40d194576bff4336c488ee85889d9b352e73dc
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-02-27 10:50:35 +1300

    today is a good day

---
+++ week 1 work/week 1 work/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_1_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("please enter your age what ever it may be");
+            age = int.Parse (Console.ReadLine());
+
+            Console.WriteLine(age);
+            Console.ReadLine();  
+
+
+        }
+    }
+}
---
+++ week 1 work/week 1 work/next tutorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_1_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name; 
+            Console.WriteLine('please enter you name')
+                age.
+
+            Console.WriteLine("please enter your age what ever it may be");
+            age = int.Parse (Console.ReadLine());
+
+            Console.WriteLine(age);
+            Console.ReadLine();  
+
+
+        }
+    }
+}
